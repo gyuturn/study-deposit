95f2eaeece20d0d8146cbdc0f881877852779f43
gyuturn <rbtjrrbals@gmail.com>
2023-03-07 18:37:27 +0900

    #2 feat: 로그인 라우팅 설정

---
+++ front/src/router/router.js
@@ -0,0 +1,12 @@
+import { createWebHistory, createRouter } from "vue-router";
+import KakaoLogin from "@/components/login/KakaoLogin.vue"
+
+const router = createRouter({
+    history : createWebHistory(),
+    routes : [ // path별 component를 추가한다.
+        { path : "/", name : "", component : "" },
+        { path : "/login", name : "kakak-login", component : KakaoLogin },
+    ]
+});
+
+export default router;
