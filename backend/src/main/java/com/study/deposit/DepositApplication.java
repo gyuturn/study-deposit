587c75e2f3b0a4df2076d3bba138318584b5f0a3
gyuturn <rbtjrrbals@gmail.com>
2023-03-11 15:29:19 +0900

    feat: 스프링부트 시간 서울존으로 설정

--- backend/src/main/java/com/study/deposit/DepositApplication.java
+++ backend/src/main/java/com/study/deposit/DepositApplication.java
@@ -1,11 +1,16 @@
 package com.study.deposit;
 
+import java.util.TimeZone;
+import javax.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DepositApplication {
-
+	@PostConstruct
+	public void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(DepositApplication.class, args);
 	}
