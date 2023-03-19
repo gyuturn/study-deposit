799f0f83f6e4833e97a7d7895435531d05de458c
gyuturn <rbtjrrbals@gmail.com>
2023-03-19 16:45:21 +0900

    fix: test db 로컬 mysql로 변경

--- backend/src/test/java/com/study/deposit/domain/user/dao/UserDaoTest.java
+++ backend/src/test/java/com/study/deposit/domain/user/dao/UserDaoTest.java
@@ -10,12 +10,14 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class UserDaoTest {
     @Autowired
-    private UserDao userDao;
+    UserDao userDao;
 
     @Test
     @DisplayName("유저 변경 로직 dao")
