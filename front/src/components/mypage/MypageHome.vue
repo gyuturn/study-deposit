9d2d95a124ffa5d6e6af9e041a995d888d225f9e
gyuturn <rbtjrrbals@gmail.com>
2023-03-28 18:13:27 +0900

    #11: 마이페이지 링크와 관련된 구조 변경

--- front/src/components/mypage/MypageHome.vue
+++ front/src/components/mypage/MypageHome.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container fluid class="fill-height justify-center align-center">
-    <v-card elevation="0"  height="100%" width="100%">
+    <v-card elevation="0" height="100%" width="100%">
       <v-card-title
         class="text-center mb-3 font-weight-bold text--primary text--body-2"
         >나의 프로필</v-card-title
@@ -12,9 +12,9 @@
               <v-list-item-title>
                 <img src="@/assets/person.png" />
               </v-list-item-title>
-              <v-list-item-subtitle class="emphasized-text"
-                >{{ nickname }}</v-list-item-subtitle
-              >
+              <v-list-item-subtitle class="emphasized-text">{{
+                nickname
+              }}</v-list-item-subtitle>
               <v-list-item-subtitle class="emphasized-text"
                 >포인트: {{ sumOfChargeAmount }}</v-list-item-subtitle
               >
@@ -23,22 +23,31 @@
           </v-list-item>
         </v-list>
         <v-list>
-          <v-list-item v-for="(item, index) in listItems" :key="index">
-            <v-list-item-content>
-              <div class="d-flex align-center justify-space-between">
-                <v-list-item-title>
-                  {{ item.title }}
-                </v-list-item-title>
-                <img src="@/assets/right_btn.png" />
-              </div>
-              <v-divider></v-divider>
-            </v-list-item-content>
-          </v-list-item>
+          <router-link
+            v-for="(item, index) in listItems"
+            :key="index"
+            :to="item.to"
+             style="color: black; text-decoration: none;"
+          >
+            <v-list-item>
+              <v-list-item-content>
+                <div
+                  class="d-flex align-center justify-space-between"
+                  style="width: 100%"
+                >
+                  <v-list-item-title>{{ item.title }}</v-list-item-title>
+                  <img src="@/assets/right_btn.png" />
+                </div>
+                <v-divider></v-divider>
+              </v-list-item-content>
+            </v-list-item>
+          </router-link>
         </v-list>
       </v-card-text>
     </v-card>
   </v-container>
 </template>
+
 
 <script>
 import axios from "axios";
@@ -51,32 +60,32 @@ export default {
         withCredentials: true, // Include cookies in the request
       })
       .then((response) => {
-          console.log(response.status);
+        console.log(response.status);
 
         if (response.status === 200) {
-          this.nickname=response.data.data.nickName;
-          this.sumOfChargeAmount=`${response.data.data.sumOfChargeAmount}원`;
-          this.email=response.data.data.email;
-        } 
+          this.nickname = response.data.data.nickName;
+          this.sumOfChargeAmount = `${response.data.data.sumOfChargeAmount}원`;
+          this.email = response.data.data.email;
+        }
       })
       .catch((error) => {
-         //  로그인 안되어 있음
-          this.$router.push({ path: "/login/kakao" });
+        //  로그인 안되어 있음
+        this.$router.push({ path: "/login/kakao" });
       });
   },
   methods: {},
   data() {
     return {
       listItems: [
-        { title: "회원정보 수정" },
-        { title: "스터디방 관리" },
-        { title: "포인트 충전하기" },
-        { title: "포인트 환전하기" },
-        { title: "로그아웃" },
-      ],
+      { title: "회원정보 수정", to: "/edit-profile" },
+      { title: "스터디방 관리", to: "/study-room-management" },
+      { title: "포인트 충전하기", to: "/payment/kakaopay" },
+      { title: "포인트 환전하기", to: "/exchange-points" },
+      { title: "로그아웃", to: "/logout" },
+    ],
       nickname: "",
-      sumOfChargeAmount:"",
-      email:""
+      sumOfChargeAmount: "",
+      email: "",
     };
   },
 };
@@ -88,4 +97,5 @@ export default {
   color: black;
   margin: 10px;
 }
+
 </style>
