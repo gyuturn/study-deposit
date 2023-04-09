5d47a59bc700dca7e7acbf85c2f949831679a08c
gyuturn <rbtjrrbals@gmail.com>
2023-04-09 20:04:51 +0900

    #11 test: 결제완료 api 테스트

--- backend/src/test/java/com/study/deposit/domain/point/controller/KaKaoPayControllerTest.java
+++ backend/src/test/java/com/study/deposit/domain/point/controller/KaKaoPayControllerTest.java
@@ -1,7 +1,10 @@
 package com.study.deposit.domain.point.controller;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
@@ -10,7 +13,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.study.deposit.domain.point.domain.PointRecord;
+import com.study.deposit.domain.point.dto.PointRecordCompleteDto;
 import com.study.deposit.domain.point.dto.PointRecordPrepareDto;
+import com.study.deposit.domain.point.dto.PointRecordResultDto;
 import com.study.deposit.domain.point.service.IamPortService;
 import com.study.deposit.domain.point.service.PointRecordService;
 import com.study.deposit.domain.user.controller.UsersController;
@@ -20,6 +26,7 @@ import com.study.deposit.domain.user.domain.Users;
 import com.study.deposit.domain.user.service.AuthService;
 import com.study.deposit.global.config.annotation.WithAuthUser;
 import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.UUID;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -36,13 +43,16 @@ import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 @WebMvcTest(controllers = KaKaoPayController.class)
 @ExtendWith(MockitoExtension.class)
 @AutoConfigureMockMvc
 @MockBean(JpaMetamodelMappingContext.class)
 class KaKaoPayControllerTest {
+    private static final String TEST_TOKEN = "test_token";
 
     @Autowired
     MockMvc mockMvc;
@@ -58,22 +68,15 @@ class KaKaoPayControllerTest {
 
 
     @Test
-    @WithAuthUser(email = "test@naver.com",role = "ROLE_USER")
+    @WithAuthUser(email = "test@naver.com", role = "ROLE_USER")
     @DisplayName("사전결제 API 테스트(성공)")
     void paymentPrepare() throws Exception {
         //given
-        PointRecordPrepareDto testDto = new PointRecordPrepareDto();
-        testDto.setAmount(1000L);
-        testDto.setMerchant_uid(UUID.randomUUID().toString());
+        PointRecordPrepareDto testDto = getPointRecordPrepareDto();
 
-        Users testUser = Users.builder()
-                .nickName("test_user")
-                .loginType(LoginType.KAKAO)
-                .email("test@naver.com")
-                .role(Role.USER)
-                .id(UUID.randomUUID()).build();
+        Users testUser = makeTestUser();
 
-        String testToken = "test_token";
+        String testToken = TEST_TOKEN;
         when(iamPortService.getToken()).thenReturn(testToken);
         when(authService.getUser()).thenReturn(testUser);
 
@@ -89,6 +92,65 @@ class KaKaoPayControllerTest {
         verify(iamPortService).getToken();
         verify(iamPortService).paymentPrepare(testDto, testToken);
         verify(pointRecordService).insertRecord(testUser, testDto);
+    }
+
+    private Users makeTestUser() {
+        Users testUser = Users.builder()
+                .nickName("test_user")
+                .loginType(LoginType.KAKAO)
+                .email("test@naver.com")
+                .role(Role.USER)
+                .id(UUID.randomUUID()).build();
+        return testUser;
+    }
+
+    private PointRecordPrepareDto getPointRecordPrepareDto() {
+        PointRecordPrepareDto testDto = new PointRecordPrepareDto();
+        testDto.setAmount(1000L);
+        testDto.setMerchant_uid(UUID.randomUUID().toString());
+        return testDto;
+    }
+
+    @Test
+    @WithAuthUser(email = "test@naver.com", role = "ROLE_USER")
+    @DisplayName("결제 최종 완료 API 테스트[성공]")
+    void paymentComplete_validPayment() throws Exception {
+        // dto 생성
+        PointRecordCompleteDto testDto = new PointRecordCompleteDto("test_imp_uid", "test_merchant_uid", "paid");
+
+        // iamport test token
+        String testToken = TEST_TOKEN;
+        when(iamPortService.getToken()).thenReturn(testToken);
+
+        //실제 iamport에서 기록된 결제 금액
+        Long actualCharge = 1000L;
+        PointRecordResultDto pointRecordResultDto = new PointRecordResultDto(actualCharge, "paid");
+        when(iamPortService.paymentInfo(testDto.getImp_uid(), testToken)).thenReturn(pointRecordResultDto);
+
+        PointRecord pointRecordInDb = PointRecord.builder()
+                .merchant_uid(testDto.getMerchant_uid())
+                .users(makeTestUser())
+                .chargeDate(LocalDateTime.now())
+                .chargeAmount(actualCharge).build();
+
+        when(pointRecordService.findByMerchantId(testDto.getMerchant_uid())).thenReturn(pointRecordInDb);
+        when(iamPortService.validPayment(pointRecordResultDto.getChargeAmount(), pointRecordInDb)).thenReturn(true);
+
+        //when
+        mockMvc.perform(post("/api/v2/point/record/kakaopay/payment/complete")
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(toJson(testDto)))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+
+        // Verify parameter
+        verify(iamPortService).getToken();
+        verify(iamPortService).paymentInfo(eq(testDto.getImp_uid()), eq(testToken));
+        verify(pointRecordService).findByMerchantId(eq(testDto.getMerchant_uid()));
+        verify(iamPortService).validPayment(eq(1000L), eq(pointRecordInDb));
+        verify(pointRecordService, never()).deleteRecord(pointRecordInDb);
+        verify(iamPortService, never()).rollbackPayment(any(), any(), any());
     }
 
     private String toJson(Object object) {
