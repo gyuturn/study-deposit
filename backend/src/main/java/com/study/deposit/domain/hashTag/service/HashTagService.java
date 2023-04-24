6bfd282f2267b759a706c7a91665414afe0e972c
gyuturn <rbtjrrbals@gmail.com>
2023-04-24 17:25:50 +0900

    #29 feat: 해시태그 생성 로직

--- backend/src/main/java/com/study/deposit/domain/hashTag/service/HashTagService.java
+++ backend/src/main/java/com/study/deposit/domain/hashTag/service/HashTagService.java
@@ -1,11 +1,14 @@
 package com.study.deposit.domain.hashTag.service;
 
+import com.study.deposit.domain.hashTag.dao.HashTagDao;
 import com.study.deposit.domain.hashTag.dao.StudyRoomHashTagDao;
 import com.study.deposit.domain.hashTag.domain.HashTag;
 import com.study.deposit.domain.hashTag.domain.StudyRoomHashTag;
+import com.study.deposit.domain.hashTag.dto.MakeHashTagReqDto;
 import com.study.deposit.domain.studyRoom.domain.StudyRoom;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -16,6 +19,8 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class HashTagService {
     private final StudyRoomHashTagDao studyRoomHashTagDao;
+    private final HashTagDao hashTagDao;
+
 
 
     @Transactional
@@ -25,5 +30,17 @@ public class HashTagService {
             studyRoomHashTags.add(StudyRoomHashTag.toEntity(savedStudyRoom, hashTag));
         }
         studyRoomHashTagDao.saveAll(studyRoomHashTags);
+    }
+
+    /**
+     * 해시태그 생성
+     */
+    @Transactional
+    public void makeHashTag(MakeHashTagReqDto makeHashTagReqDto) {
+        hashTagDao.save(HashTag.toEntity(makeHashTagReqDto));
+    }
+
+    private boolean isDupHashTag(String tagName) {
+        return hashTagDao.findByTagName(tagName).isPresent();
     }
 }
