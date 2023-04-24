ec202a58bed8e4950ccaa5aa2c3dd52fd8dfd662
gyuturn <rbtjrrbals@gmail.com>
2023-04-24 17:52:01 +0900

    #29 test: 스터디방 생성 시 해스태그 매핑 로직

--- backend/src/test/java/com/study/deposit/domain/hashTag/service/HashTagServiceTest.java
+++ backend/src/test/java/com/study/deposit/domain/hashTag/service/HashTagServiceTest.java
@@ -5,8 +5,17 @@ import static org.mockito.Mockito.*;
 import static org.mockito.Mockito.verify;
 
 import com.study.deposit.domain.hashTag.dao.HashTagDao;
+import com.study.deposit.domain.hashTag.dao.StudyRoomHashTagDao;
 import com.study.deposit.domain.hashTag.domain.HashTag;
+import com.study.deposit.domain.hashTag.domain.StudyRoomHashTag;
 import com.study.deposit.domain.hashTag.dto.MakeHashTagReqDto;
+import com.study.deposit.domain.studyRoom.domain.AttendanceType;
+import com.study.deposit.domain.studyRoom.domain.StudyRoom;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -19,6 +28,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 class HashTagServiceTest {
     @Mock
     private HashTagDao hashTagDao;
+    @Mock
+    private StudyRoomHashTagDao studyRoomHashTagDao;
 
     @InjectMocks
     private HashTagService hashTagService;
@@ -32,5 +43,37 @@ class HashTagServiceTest {
         hashTagService.makeHashTag(dto);
 
         verify(hashTagDao).save(any(HashTag.class));
+    }
+
+    @Test
+    @DisplayName("스터디방 생성시 해당 해시태그 매핑 및 DB저장")
+    void hashTagsMapStudyRoom() {
+        HashTag tag1 = HashTag.builder()
+                .id(1L)
+                .tagName("tag1")
+                .build();
+        HashTag tag2 = HashTag.builder()
+                .id(2L)
+                .tagName("tag2")
+                .build();
+        List<HashTag> hashTags = new ArrayList<>();
+        hashTags.add(tag1);
+        hashTags.add(tag2);
+
+        StudyRoom studyRoom = StudyRoom.builder()
+                .id(3L)
+                .endDate(LocalDate.of(2024, 04, 01))
+                .attendanceTime(LocalTime.of(13, 00))
+                .personCapacity(10L)
+                .deposit(1000L)
+                .createDate(LocalDateTime.of(2023, 04, 01, 3, 30))
+                .attendanceType(AttendanceType.AttendanceCheck)
+                .title("testStudyRoom")
+                .build();
+
+        hashTagService.hashTagsMapStudyRoom(hashTags, studyRoom);
+
+        verify(studyRoomHashTagDao, times(1)).saveAll(Mockito.any(List.class));
+
     }
 }
