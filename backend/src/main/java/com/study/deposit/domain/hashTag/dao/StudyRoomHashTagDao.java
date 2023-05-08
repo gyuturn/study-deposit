8a7d18fe56c382b4ae93285546b79951d5eb32e9
gyuturn <rbtjrrbals@gmail.com>
2023-05-08 15:29:35 +0900

    #33 feat: 스터디방 전체 조회

--- backend/src/main/java/com/study/deposit/domain/hashTag/dao/StudyRoomHashTagDao.java
+++ backend/src/main/java/com/study/deposit/domain/hashTag/dao/StudyRoomHashTagDao.java
@@ -2,7 +2,9 @@ package com.study.deposit.domain.hashTag.dao;
 
 import com.study.deposit.domain.hashTag.domain.StudyRoomHashTag;
 import com.study.deposit.domain.studyRoom.domain.StudyRoom;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StudyRoomHashTagDao extends JpaRepository<StudyRoomHashTag, Long> {
+    List<StudyRoomHashTag> findByStudyRoom(StudyRoom studyRoom);
 }
--- backend/src/main/java/com/study/deposit/domain/studyRoom/dao/UserStudyRoomDao.java
+++ backend/src/main/java/com/study/deposit/domain/studyRoom/dao/UserStudyRoomDao.java
@@ -1,9 +1,12 @@
 package com.study.deposit.domain.studyRoom.dao;
 
+import com.study.deposit.domain.hashTag.domain.StudyRoomHashTag;
 import com.study.deposit.domain.studyRoom.domain.StudyRoom;
 import com.study.deposit.domain.studyRoom.domain.UserStudyRoom;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserStudyRoomDao extends JpaRepository<UserStudyRoom, Long> {
+    List<UserStudyRoom> findByStudyRoom(StudyRoom studyRoom);
 }
 
