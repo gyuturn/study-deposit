7200c0eb2095a0cc2ecebd9f5637610891f78e0c
gyuturn <rbtjrrbals@gmail.com>
2023-05-11 14:12:40 +0900

    #42 feat: 버튼 링크 기능 추가

--- front/src/components/study_room/enter/StudyRoomLink.vue
+++ front/src/components/study_room/enter/StudyRoomLink.vue
@@ -1,23 +1,21 @@
 <template>
   <div>
-          <!-- 상단 동그란 박스 -->
-          <v-row justify="center" class="mb-4">
-            <v-col cols="12" sm="8" md="6">
-              <v-card class="text-center">
-                <v-title>
-                    스터디방 제목
-                </v-title>
-                <v-card-actions>
-                   <!-- 출석하기 링크 -->
-                  <v-btn  class="link" @click="goTo('attendance')">출석하기</v-btn>
-                  <!-- 방정보 링크 -->
-                  <v-btn  class="link" @click="goTo('room-info')">방정보</v-btn>
-                  <!-- 채팅 링크 -->
-                  <v-btn  class="link" @click="goTo('chat')">채팅</v-btn>
-                </v-card-actions>
-              </v-card>
-            </v-col>
-          </v-row>
+    <!-- 상단 동그란 박스 -->
+    <v-row justify="center" class="mb-4">
+      <v-col cols="12" sm="8" md="6">
+        <v-card class="text-center">
+          <v-title> 스터디방 제목 </v-title>
+          <v-card-actions class="link-btn">
+            <!-- 출석하기 링크 -->
+            <v-btn class="link" @click="goTo('attendance')">출석하기</v-btn>
+            <!-- 방정보 링크 -->
+            <v-btn class="link" @click="goTo('info')">방정보</v-btn>
+            <!-- 채팅 링크 -->
+            <v-btn class="link" @click="goTo('chat')">채팅</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
@@ -26,12 +24,40 @@ import axios from "axios";
 
 export default {
   name: "StudyRoomLink",
-}
+  methods: {
+    goTo(route) {
+      const currentUri = this.$route.path;
+      const parts = currentUri.split("/");
+      const lastPart = parts[parts.length - 1];
+      const number = parseInt(lastPart);
+      // 각 링크 클릭 시 실행될 동작을 추가하세요.
+      switch (route) {
+        case "attendance":
+          this.$router.push({ path: "/studyroom/attendance/" + number });
+          break;
+        case "room-info":
+          this.$router.push({ path: "/studyroom/info/" + number });
+          break;
+        case "chat":
+          this.$router.push({ path: "/studyroom/chat/" + +number });
+          break;
+        default:
+          break;
+      }
+    },
+  },
+};
 </script>
 <style>
 .link {
-  background-color: #006FFD;
-  color:white;
+  background-color: #006ffd;
+  color: white;
   border-radius: 30px;
+  margin: 10px;
+}
+.link-btn {
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
