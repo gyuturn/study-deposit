a5498f6eb493e346061a88b82493c0fec448a352
gyuturn <rbtjrrbals@gmail.com>
2023-05-17 21:36:36 +0900

    #54 feat: pwa 수정

--- front/src/main.js
+++ front/src/main.js
@@ -4,6 +4,7 @@ import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
 import router from "./router/router.js"
 import './registerServiceWorker.js'
+import './registerServiceWorker'
 
 loadFonts()
 
